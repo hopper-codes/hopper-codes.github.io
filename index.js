7d2e0481ceba22b2fc8889ed185323a23217b9d5
cmlacrd <camilacardoso96@hotmail.com>
2020-07-26 01:22:43 -0300

    new logo and pallette of colors

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+
+
 function sendMail(formEmail){
  var data = formEmail;
  var  status = null;
 
-  emailjs.sendForm('default_service', 'email_hopper_codes', data);
-
-  alert('Obrigada por entrar em contato comigo. Sua mensagem foi enviada!');
+ emailjs.sendForm('default_service', 'email_hopper_codes', data);
+ 
+ alert('Obrigada por entrar em contato comigo. Sua mensagem foi enviada!');
 
   return false;
-
 }
